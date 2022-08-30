40c6810155c170b404238034e0ba5390c7aecb55
Aman Salykov <amanzholsv@gmail.com>
2022-08-30 17:06:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+"""
+Machine Vision and Cognitive Robotics
+Matthias Hirschmanner 2020
+Automation & Control Institute, TU Wien
+"""
 
 from pathlib import Path
 
